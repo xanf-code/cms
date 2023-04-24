e9990520773cb83a44269d35b4867af350ab1658
xanf-code <darshanaswathh@gmail.com>
2023-04-24 20:39:00 +0530

    Sorting

--- src/main/java/com/democms/cms/controller/BlogController.java
+++ src/main/java/com/democms/cms/controller/BlogController.java
@@ -6,7 +6,6 @@ import com.democms.cms.repository.BlogRepository;
 import com.democms.cms.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
