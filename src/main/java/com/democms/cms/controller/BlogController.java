c7321f00480b645ca5c61fc0abdff6409323dfec
xanf-code <darshanaswathh@gmail.com>
2023-04-24 20:38:02 +0530

    Sorting

--- src/main/java/com/democms/cms/controller/BlogController.java
+++ src/main/java/com/democms/cms/controller/BlogController.java
@@ -5,11 +5,12 @@ import com.democms.cms.entity.User;
 import com.democms.cms.repository.BlogRepository;
 import com.democms.cms.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
+import java.util.*;
 
 @RestController
 public class BlogController {
@@ -20,7 +21,23 @@ public class BlogController {
     private UserRepository userRepository;
 
     @PostMapping("/create/blog")
-    public Blog createBlog(@RequestBody Blog blogRequest) {
+    Blog createBlog(@RequestBody Blog blogRequest) {
         return blogRepository.save(blogRequest);
+    }
+
+    @GetMapping("/v2/blogs")
+    ResponseEntity<Map<String,?>> getBlogs(@RequestParam("id") Long user_id){
+       Optional<User> user = userRepository.findById(user_id);
+        if(user.isPresent()){
+            List<Blog> blogs = user.get().getBlogs();
+            blogs.sort(Comparator.comparing(Blog::getBlog_id, Comparator.reverseOrder()));
+            Map<String, Object> response = new HashMap<>();
+            response.put("blogs",blogs);
+            return ResponseEntity.status(HttpStatus.FOUND).body(response);
+        } else {
+            Map<String, String> response = new HashMap<>();
+            response.put("message","No Blogs found.");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        }
     }
 }
